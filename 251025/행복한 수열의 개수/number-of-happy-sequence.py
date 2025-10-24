f5fc3a9d54cfccc360a473d068e3e52453df8196
c.10 <siyul_sung@korea.ac.kr>
2025-10-25 00:26:54 +0900

    Title: 행복한 수열의 개수, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 251025/행복한 수열의 개수/number-of-happy-sequence.py
+++ 251025/행복한 수열의 개수/number-of-happy-sequence.py
@@ -10,15 +10,8 @@ def is_happy_arr(arr, m):
             cnt += 1
         else:
             cnt = 1
-        # if cnt >= m:
-        #     return 1
         max_cnt = max(max_cnt, cnt)
     return max_cnt >= m
-
-# def is_happy_arr(arr, m):
-#     cons_cnt, max_cnt = 1, 1
-#     for i in range(len(arr)):
-#         if arr[i-1] == 
 
 # 각 행마다
 fin_cnt = 0
@@ -30,12 +23,6 @@ _grid = list(zip(*grid))
 # # 각 열마다
 for col in _grid:
     fin_cnt += is_happy_arr(col, m)
-
-# col = [0] * n
-# for j in range(n):
-#     for i in range(n):
-#         col[i] = grid[i][j]
-#     fin_cnt += is_happy_arr(col, m)
 
 print(fin_cnt)
 
