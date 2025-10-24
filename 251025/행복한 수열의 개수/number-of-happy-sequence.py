3b216a0a91d668ae0d09f9ff8ca0fd4c7d6e6c10
c.10 <siyul_sung@korea.ac.kr>
2025-10-25 00:24:47 +0900

    Title: 행복한 수열의 개수, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

--- 251025/행복한 수열의 개수/number-of-happy-sequence.py
+++ 251025/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,15 +4,21 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 # 해당 수열이 동일 원소가 M개 이상 연속으로 나오는가?
 def is_happy_arr(arr, m):
-    cnt = 1
+    max_cnt, cnt = 1, 1
     for i in range(1, len(arr)):
         if arr[i-1] == arr[i]:
             cnt += 1
         else:
             cnt = 1
-        if cnt >= m:
-            return 1
-    return 0
+        # if cnt >= m:
+        #     return 1
+        max_cnt = max(max_cnt, cnt)
+    return max_cnt >= m
+
+# def is_happy_arr(arr, m):
+#     cons_cnt, max_cnt = 1, 1
+#     for i in range(len(arr)):
+#         if arr[i-1] == 
 
 # 각 행마다
 fin_cnt = 0
