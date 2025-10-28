b60bf028083821cff29d5374c48c9f8b0c52d0ff
c.10 <siyul_sung@korea.ac.kr>
2025-10-28 14:52:00 +0900

    Title: 1이 되는 순간까지, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251028/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+# Please write your code here.
+
+def f(N):
+    if N == 1:
+        return 0
+    if N % 2 == 0:
+        return 1 + f(N//2)
+    else:
+        return 1 + f(N//3)
+
+print(f(N))
