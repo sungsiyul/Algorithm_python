63c65118095185488c771f1c17cfd5475865fdce
c.10 <siyul_sung@korea.ac.kr>
2025-11-08 02:09:38 +0900

    Title: Top K 숫자 구하기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251108/Top K 숫자 구하기/kth-number.py
+++ 251108/Top K 숫자 구하기/kth-number.py
@@ -2,5 +2,13 @@ n, k = map(int, input().split())
 nums = list(map(int, input().split()))
 
 # Please write your code here.
-nums.sort(reverse=True)
-print(nums[k-1])
+import heapq as hq
+
+heap = []
+for num in nums:
+    hq.heappush(heap, num)
+
+for _ in range(k-1):
+    hq.heappop(heap)
+
+print(heap[0])
