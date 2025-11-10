58e689680623a3a3f1a8b0e776ccf91c5e083469
c.10 <siyul_sung@korea.ac.kr>
2025-11-11 00:56:49 +0900

    Title: DateTime to DateTime, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251111/DateTime to DateTime/datetime-to-datetime.py
+++ 251111/DateTime to DateTime/datetime-to-datetime.py
@@ -4,4 +4,6 @@ a, b, c = map(int, input().split())
 def get_minutes(a, b, c):
     return a * 24 * 60 + b * 60 + c
 
-print(get_minutes(a, b, c) - get_minutes(11, 11, 11))
+time = get_minutes(a, b, c) - get_minutes(11, 11, 11)
+
+print(time if time > 0 else -1)
